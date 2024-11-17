b164f93fdf3a41461d751cdadaad3cc1e1f8f70d
jschwabe <mail@jschwabe.site>
2024-11-18 00:14:39 +0100

    chore: clean up by using builtin functions, reduce number of params (index_of)
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- get_next_line.rs
+++ get_next_line.rs
@@ -1,3 +1,6 @@
+#![allow(static_mut_refs)]
+use std::ptr::copy_nonoverlapping;
+
 use ::libc;
 use libft_rs::{
 	ft_calloc::ft_calloc, ft_memcpy::ft_memcpy, ft_memset::ft_memset, ft_strlcpy::ft_strlcpy,
@@ -8,21 +11,25 @@ extern "C" {
 	fn malloc(_: libc::c_ulong) -> *mut libc::c_void;
 }
 
+///
+/// # Safety
+/// This function is unsafe because it dereferences raw pointers.
+/// Takes a string, a character, and a maximum length.
+/// Returns the index of the character in the string.
+/// If the character is not found, returns the maximum length.
 #[no_mangle]
 pub unsafe extern "C" fn index_of(
 	mut str: *mut libc::c_char,
-	mut c: libc::c_char,
 	mut max_len: libc::c_int,
 ) -> libc::c_int {
 	let mut i: libc::c_int = 0;
-	i = 0 as libc::c_int;
 	while i < max_len
-		&& *str.offset(i as isize) as libc::c_int != c as libc::c_int
+		&& *str.offset(i as isize) as libc::c_int != '\n' as libc::c_int
 		&& *str.offset(i as isize) as libc::c_int != '\0' as i32
 	{
 		i += 1;
 	}
-	return i;
+	i
 }
 
 pub type size_t = libc::c_ulong;
@@ -41,24 +48,32 @@ unsafe extern "C" fn check_free(
 		return std::ptr::null_mut::<libc::c_char>();
 	}
 	if is_buf {
-		buf_nl_idx = index_of(buf, '\n' as i32 as libc::c_char, 2147483647 as libc::c_int);
-		ft_memcpy(
-			line as *mut libc::c_void,
-			buf as *const libc::c_void,
-			(buf_idx + 1 as libc::c_int) as size_t,
-		);
+		buf_nl_idx = index_of(buf, 2147483647 as libc::c_int);
+		// ft_memcpy(
+		// 	line as *mut libc::c_void,
+		// 	buf as *const libc::c_void,
+		// 	(buf_idx + 1 as libc::c_int) as size_t,
+		// );
+		copy_nonoverlapping(buf, line, (buf_idx + 1).try_into().unwrap());
 		if *buf.offset(buf_nl_idx as isize) as libc::c_int != '\n' as i32 {
 			*buf.offset(buf_nl_idx as isize) = 0 as libc::c_int as libc::c_char;
 		} else {
 			buf_nl_idx += 1;
 		}
-		ft_memcpy(
-			buf as *mut libc::c_void,
+		// ft_memcpy(
+		// 	buf as *mut libc::c_void,
+		// 	buf.offset(buf_nl_idx as isize) as *const libc::c_void,
+		// 	(16 as libc::c_int - buf_nl_idx + 1 as libc::c_int) as size_t,
+		// );
+		copy_nonoverlapping(
 			buf.offset(buf_nl_idx as isize) as *const libc::c_void,
-			(16 as libc::c_int - buf_nl_idx + 1 as libc::c_int) as size_t,
+			buf as *mut libc::c_void,
+			(16 as libc::c_int - buf_nl_idx + 1 as libc::c_int)
+				.try_into()
+				.unwrap(),
 		);
 	}
-	gnl_idx = index_of(line, '\n' as i32 as libc::c_char, 2147483647 as libc::c_int);
+	gnl_idx = index_of(line, 2147483647 as libc::c_int);
 	if *line.offset(gnl_idx as isize) as libc::c_int == '\n' as i32 {
 		gnl_idx += 1;
 	}
@@ -68,16 +83,25 @@ unsafe extern "C" fn check_free(
 	) as *mut libc::c_char;
 	if ret.is_null() {
 		free(line as *mut libc::c_void);
-		return 0 as *mut libc::c_void as *mut libc::c_char;
+		return std::ptr::null_mut::<libc::c_void>() as *mut libc::c_char;
 	}
-	ft_memcpy(
-		ret as *mut libc::c_void,
-		line as *const libc::c_void,
-		gnl_idx as size_t,
-	);
+	// ft_memcpy(
+	// 	ret as *mut libc::c_void,
+	// 	line as *const libc::c_void,
+	// 	gnl_idx as size_t,
+	// );
+	copy_nonoverlapping(line, ret, gnl_idx.try_into().unwrap());
 	free(line as *mut libc::c_void);
 	ret
 }
+
+///
+/// # Safety
+/// This function is unsafe because it dereferences raw pointers.
+/// Takes a file descriptor.
+/// Returns the next line from the file descriptor.
+/// If the file descriptor is invalid, returns a null pointer.
+/// If the line is empty, returns a null pointer.
 #[no_mangle]
 pub unsafe extern "C" fn get_next_line(mut fd: libc::c_int) -> *mut libc::c_char {
 	let mut line: *mut libc::c_char = std::ptr::null_mut::<libc::c_char>();
@@ -107,7 +131,7 @@ pub unsafe extern "C" fn get_next_line(mut fd: libc::c_int) -> *mut libc::c_char
 	if buf[buf_idx as usize] as libc::c_int != '\n' as i32 {
 		read_line(buf.as_mut_ptr(), fd, &mut buf_idx, &mut line);
 	}
-	return check_free(buf.as_mut_ptr(), buf_idx, line, 0 as libc::c_int != 0);
+	check_free(buf.as_mut_ptr(), buf_idx, line, 0 as libc::c_int != 0)
 }
 #[inline]
 unsafe extern "C" fn iter_line(
@@ -130,11 +154,7 @@ unsafe extern "C" fn iter_line(
 		tmp as *const libc::c_void,
 		16 as libc::c_int as size_t,
 	);
-	buf_nl_idx = index_of(
-		buf,
-		'\n' as i32 as libc::c_char,
-		16 as libc::c_int + 1 as libc::c_int,
-	);
+	buf_nl_idx = index_of(buf, 16 as libc::c_int + 1 as libc::c_int);
 	if *buf.offset(buf_nl_idx as isize) as libc::c_int != '\n' as i32 {
 		*buf.offset(buf_nl_idx as isize) = 0 as libc::c_int as libc::c_char;
 	} else {
@@ -145,7 +165,7 @@ unsafe extern "C" fn iter_line(
 		buf.offset(buf_nl_idx as isize) as *const libc::c_void,
 		(16 as libc::c_int - buf_nl_idx + 1 as libc::c_int) as size_t,
 	);
-	return 1 as libc::c_int != 0;
+	1 as libc::c_int != 0
 }
 unsafe extern "C" fn read_line(
 	mut buf: *mut libc::c_char,
@@ -174,11 +194,7 @@ unsafe extern "C" fn read_line(
 	if rd > 0 as libc::c_int {
 		*buf_idx += 16 as libc::c_int;
 	}
-	tmp_nl_idx = index_of(
-		tmp.as_mut_ptr(),
-		'\n' as i32 as libc::c_char,
-		16 as libc::c_int,
-	);
+	tmp_nl_idx = index_of(tmp.as_mut_ptr(), 16 as libc::c_int);
 	if (tmp[tmp_nl_idx as usize] as libc::c_int == '\n' as i32
 		|| rd == 0 as libc::c_int && *buf_idx != 0 as libc::c_int)
 		&& !iter_line(line, buf, tmp.as_mut_ptr(), *buf_idx)
@@ -193,19 +209,20 @@ unsafe extern "C" fn read_line(
 	}
 	if rd > 0 as libc::c_int && *buf_idx != 0 as libc::c_int {
 		*buf_idx -= 16 as libc::c_int;
-		tmp_nl_idx = index_of(
+		tmp_nl_idx = index_of(tmp.as_mut_ptr(), 16 as libc::c_int);
+		// ft_memcpy(
+		// 	(*line).offset(*buf_idx as isize) as *mut libc::c_void,
+		// 	tmp.as_mut_ptr() as *const libc::c_void,
+		// 	tmp_nl_idx as size_t,
+		// );
+		copy_nonoverlapping(
 			tmp.as_mut_ptr(),
-			'\n' as i32 as libc::c_char,
-			16 as libc::c_int,
-		);
-		ft_memcpy(
-			(*line).offset(*buf_idx as isize) as *mut libc::c_void,
-			tmp.as_mut_ptr() as *const libc::c_void,
-			tmp_nl_idx as size_t,
+			(*line).offset(*buf_idx as isize),
+			tmp_nl_idx.try_into().unwrap(),
 		);
 		if tmp[tmp_nl_idx as usize] as libc::c_int == '\n' as i32 {
 			*(*line).offset((*buf_idx + tmp_nl_idx) as isize) = '\n' as i32 as libc::c_char;
 		}
 	}
-	return *line;
+	*line
 }
