27aa69682ae471ada5f4874128b54043abcfcbe2
jschwabe <mail@jschwabe.site>
2024-11-18 00:28:28 +0100

    chore: remove possible unused_mut unused_assignments
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- get_next_line.rs
+++ get_next_line.rs
@@ -11,7 +11,6 @@ use libft_rs::{
 extern "C" {
 	fn free(_: *mut libc::c_void);
 	fn read(__fd: libc::c_int, __buf: *mut libc::c_void, __nbytes: size_t) -> ssize_t;
-	fn malloc(_: libc::c_ulong) -> *mut libc::c_void;
 }
 
 ///
@@ -21,10 +20,7 @@ extern "C" {
 /// Returns the index of the character in the string.
 /// If the character is not found, returns the maximum length.
 #[no_mangle]
-pub unsafe extern "C" fn index_of(
-	mut str: *mut libc::c_char,
-	mut max_len: libc::c_int,
-) -> libc::c_int {
+pub unsafe extern "C" fn index_of(str: *mut libc::c_char, max_len: libc::c_int) -> libc::c_int {
 	let mut i: libc::c_int = 0;
 	while i < max_len
 		&& *str.offset(i as isize) as libc::c_int != '\n' as libc::c_int
@@ -38,20 +34,18 @@ pub unsafe extern "C" fn index_of(
 pub type size_t = libc::c_ulong;
 pub type __ssize_t = libc::c_long;
 pub type ssize_t = __ssize_t;
+#[allow(unused_mut)]
 unsafe extern "C" fn check_free(
 	mut buf: *mut libc::c_char,
-	mut buf_idx: libc::c_int,
+	buf_idx: libc::c_int,
 	mut line: *mut libc::c_char,
-	mut is_buf: bool,
+	is_buf: bool,
 ) -> *mut libc::c_char {
-	let mut buf_nl_idx: libc::c_int = 0;
-	let mut ret: *mut libc::c_char = std::ptr::null_mut::<libc::c_char>();
-	let mut gnl_idx: libc::c_int = 0;
 	if line.is_null() {
 		return std::ptr::null_mut::<libc::c_char>();
 	}
 	if is_buf {
-		buf_nl_idx = index_of(buf, 2147483647 as libc::c_int);
+		let mut buf_nl_idx: libc::c_int = index_of(buf, 2147483647 as libc::c_int);
 		// ft_memcpy(
 		// 	line as *mut libc::c_void,
 		// 	buf as *const libc::c_void,
@@ -76,11 +70,11 @@ unsafe extern "C" fn check_free(
 				.unwrap(),
 		);
 	}
-	gnl_idx = index_of(line, 2147483647 as libc::c_int);
+	let mut gnl_idx: libc::c_int = index_of(line, 2147483647 as libc::c_int);
 	if *line.offset(gnl_idx as isize) as libc::c_int == '\n' as i32 {
 		gnl_idx += 1;
 	}
-	ret = ft_calloc(
+	let mut ret: *mut libc::c_char = ft_calloc(
 		::core::mem::size_of::<libc::c_char>() as libc::c_ulong,
 		(gnl_idx + 1 as libc::c_int) as size_t,
 	) as *mut libc::c_char;
@@ -106,15 +100,13 @@ unsafe extern "C" fn check_free(
 /// If the file descriptor is invalid, returns a null pointer.
 /// If the line is empty, returns a null pointer.
 #[no_mangle]
-pub unsafe extern "C" fn get_next_line(mut fd: libc::c_int) -> *mut libc::c_char {
-	let mut line: *mut libc::c_char = std::ptr::null_mut::<libc::c_char>();
+pub unsafe extern "C" fn get_next_line(fd: libc::c_int) -> *mut libc::c_char {
 	static mut buf: [libc::c_char; BUF_SIZE_ONE] = [0; BUF_SIZE_ONE];
-	let mut buf_idx: libc::c_int = 0;
 	if fd < 0 as libc::c_int || BUF_SIZE < 1 as libc::c_int {
 		return std::ptr::null_mut::<libc::c_char>();
 	}
-	line = std::ptr::null_mut::<libc::c_char>();
-	buf_idx = -(1 as libc::c_int);
+	let mut line: *mut libc::c_char = std::ptr::null_mut::<libc::c_char>();
+	let mut buf_idx: libc::c_int = -(1 as libc::c_int);
 	loop {
 		buf_idx += 1;
 		if !(buf_idx < BUF_SIZE && buf[buf_idx as usize] as libc::c_int != 0) {
@@ -136,14 +128,14 @@ pub unsafe extern "C" fn get_next_line(mut fd: libc::c_int) -> *mut libc::c_char
 	}
 	check_free(buf.as_mut_ptr(), buf_idx, line, 0 as libc::c_int != 0)
 }
+#[allow(unused_mut)]
 #[inline]
 unsafe extern "C" fn iter_line(
 	mut line: *mut *mut libc::c_char,
 	mut buf: *mut libc::c_char,
 	mut tmp: *mut libc::c_char,
-	mut buf_idx: libc::c_int,
+	buf_idx: libc::c_int,
 ) -> bool {
-	let mut buf_nl_idx: libc::c_int = 0;
 	*line = ft_calloc(
 		::core::mem::size_of::<libc::c_char>() as libc::c_ulong,
 		(buf_idx + 1 as libc::c_int) as size_t,
@@ -157,7 +149,7 @@ unsafe extern "C" fn iter_line(
 		tmp as *const libc::c_void,
 		BUF_SIZE as size_t,
 	);
-	buf_nl_idx = index_of(buf, BUF_SIZE + 1 as libc::c_int);
+	let mut buf_nl_idx: libc::c_int = index_of(buf, BUF_SIZE + 1 as libc::c_int);
 	if *buf.offset(buf_nl_idx as isize) as libc::c_int != '\n' as i32 {
 		*buf.offset(buf_nl_idx as isize) = 0 as libc::c_int as libc::c_char;
 	} else {
@@ -170,9 +162,10 @@ unsafe extern "C" fn iter_line(
 	);
 	1 as libc::c_int != 0
 }
+#[allow(unused_mut)]
 unsafe extern "C" fn read_line(
 	mut buf: *mut libc::c_char,
-	mut fd: libc::c_int,
+	fd: libc::c_int,
 	mut buf_idx: *mut libc::c_int,
 	mut line: *mut *mut libc::c_char,
 ) -> *mut libc::c_char {
@@ -186,7 +179,6 @@ unsafe extern "C" fn read_line(
 		),
 		BUF_SIZE as size_t,
 	) as libc::c_int;
-	let mut tmp_nl_idx: libc::c_int = 0;
 	if rd == -(1 as libc::c_int) {
 		return ft_memset(
 			buf as *mut libc::c_void,
@@ -197,7 +189,7 @@ unsafe extern "C" fn read_line(
 	if rd > 0 as libc::c_int {
 		*buf_idx += BUF_SIZE;
 	}
-	tmp_nl_idx = index_of(tmp.as_mut_ptr(), BUF_SIZE);
+	let mut tmp_nl_idx: libc::c_int = index_of(tmp.as_mut_ptr(), BUF_SIZE);
 	if (tmp[tmp_nl_idx as usize] as libc::c_int == '\n' as i32
 		|| rd == 0 as libc::c_int && *buf_idx != 0 as libc::c_int)
 		&& !iter_line(line, buf, tmp.as_mut_ptr(), *buf_idx)
