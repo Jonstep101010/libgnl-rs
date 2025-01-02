0326f35b2b1725cb6be4f2b0edcb223b119f138c
jschwabe <mail@jschwabe.site>
2025-01-02 23:42:35 +0100

    chore: add changing crate-type to build.rs
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 // build.rs
 use std::env;
 use std::fs::File;
+use std::io::Read;
 use std::io::Write;
 #[cfg(all(unix, not(target_os = "macos")))]
 fn main() {
@@ -26,6 +27,28 @@ fn main() {
 	let dest_path = Path::new(&out_dir).join("buffer_size.rs");
 	let mut file = File::create(&dest_path).unwrap();
 	writeln!(file, "pub const BUFFER_SIZE: usize = {};", buffer_size).unwrap();
+
+	let cargo_toml_path = Path::new("Cargo.toml");
+	let mut cargo_toml = String::new();
+	File::open(cargo_toml_path)
+		.unwrap()
+		.read_to_string(&mut cargo_toml)
+		.unwrap();
+	// set crate-type to rlib (prefix non-commented out, comment out static-lib in Cargo.toml)
+	#[cfg(test)]
+	let new_cargo_toml = cargo_toml.replace("crate-type = [\"staticlib\"]", "crate-type = [\"rlib\"]");
+
+	// set crate-type to static-lib (prefix non-commented out, comment out rlib in Cargo.toml)
+	#[cfg(not(test))]
+	let new_cargo_toml = cargo_toml.replace("crate-type = [\"rlib\"]", "crate-type = [\"staticlib\"]");
+	// Write the updated Cargo.toml back to the file
+
+	let mut file = std::fs::OpenOptions::new()
+		.write(true)
+		.truncate(true)
+		.open(cargo_toml_path)
+		.unwrap();
+	file.write_all(new_cargo_toml.as_bytes()).unwrap();
 
 	println!("cargo:rerun-if-env-changed=BUFFER_SIZE");
 }
