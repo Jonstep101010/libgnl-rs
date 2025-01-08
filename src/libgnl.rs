2cc706665a1e0945e0a5a1f8abd4538e58b000a8
Jonstep101010 <mail@jschwabe.site>
2025-01-08 23:06:36 +0100

    chore: cleanup feature usage

--- src/libgnl.rs
+++ src/libgnl.rs
@@ -2,11 +2,6 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 #![feature(extern_types)]
-#![feature(cstr_bytes)]
 #![feature(clone_to_uninit)]
-#![feature(slice_as_array)]
-#![feature(box_into_boxed_slice)]
-#![feature(ptr_as_ref_unchecked)]
-#![feature(slice_ptr_get)]
 extern crate libc;
 pub mod c2rust_gnl;
