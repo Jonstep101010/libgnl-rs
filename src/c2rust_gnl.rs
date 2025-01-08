8a9ea1ae1bfd28888787a054becfc75fd8a7a90c
jschwabe <mail@jschwabe.site>
2025-01-08 20:18:33 +0100

    chore: added ManuallyDrop
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- src/c2rust_gnl.rs
+++ src/c2rust_gnl.rs
@@ -5,6 +5,7 @@
 use std::{
 	clone::CloneToUninit,
 	ffi::{c_char, c_int, c_ulong},
+	mem::ManuallyDrop,
 	os::fd::RawFd,
 };
 const ALLOC_SIZE: c_ulong = core::mem::size_of::<u8>() as c_ulong;
@@ -26,8 +27,8 @@ fn read_newln(
 	fd: RawFd,
 	count: &mut usize,
 	static_buffer: &mut [u8; BUFFER_SIZE + 1],
-	mut return_line: Option<*mut u8>,
-) -> Option<*mut u8> {
+	mut return_line: Option<ManuallyDrop<Vec<u8>>>,
+) -> Option<ManuallyDrop<Vec<u8>>> {
 	let mut read_buffer: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
 	let read_result = nix::unistd::read(fd, read_buffer.as_mut_slice());
 	if read_result.is_err() || read_result.unwrap() == 0 && *count == 0 {
@@ -39,15 +40,17 @@ fn read_newln(
 		if let Some(newline_pos) = nl_position(&read_buffer[..]) {
 			let mut alloc_nln = vec![0; *count + BUFFER_SIZE + 1];
 			static_buffer.as_slice().clone_into(&mut alloc_nln);
-			return_line = Some(alloc_nln.as_mut_ptr());
-			std::mem::forget(alloc_nln);
+			return_line = Some(ManuallyDrop::new(alloc_nln));
 			static_buffer[..BUFFER_SIZE].copy_from_slice(&read_buffer[..]);
 			static_buffer.copy_within(newline_pos + 1.., 0);
 			static_buffer[(BUFFER_SIZE - newline_pos)..].fill(b'\0');
 			unsafe {
 				read_buffer[..newline_pos + 1]
 					.as_ptr()
-					.copy_to_nonoverlapping(return_line.unwrap().add(*count), newline_pos + 1);
+					.copy_to_nonoverlapping(
+						return_line.as_mut().unwrap().as_mut_ptr().add(*count),
+						newline_pos + 1,
+					);
 			}
 		} else
 		/* there is a remainder for the line */
@@ -56,9 +59,10 @@ fn read_newln(
 			return_line = read_newln(fd, count, static_buffer, return_line);
 			*count -= BUFFER_SIZE;
 			unsafe {
-				read_buffer[..BUFFER_SIZE]
-					.as_ptr()
-					.copy_to_nonoverlapping(return_line.unwrap().add(*count), BUFFER_SIZE);
+				read_buffer[..BUFFER_SIZE].as_ptr().copy_to_nonoverlapping(
+					return_line.as_mut().unwrap().as_mut_ptr().add(*count),
+					BUFFER_SIZE,
+				);
 			}
 		}
 	} else
@@ -67,8 +71,7 @@ fn read_newln(
 		assert!(!static_buffer.contains(&b'\n'));
 		let mut alloc_nul = vec![0; *count + 1];
 		static_buffer.as_slice().clone_into(&mut alloc_nul);
-		return_line = Some(alloc_nul.as_mut_ptr());
-		std::mem::forget(alloc_nul);
+		return_line = Some(ManuallyDrop::new(alloc_nul));
 		static_buffer.fill(b'\0');
 	}
 	return_line
@@ -115,18 +118,19 @@ pub unsafe extern "C" fn get_next_line(fd: RawFd) -> *mut c_char {
 	if count <= BUFFER_SIZE && static_buffer[fd][count] == b'\n' {
 		return read_buffer(&mut (static_buffer[fd]), count);
 	}
-	if let Some(line) = read_newln(
+	if let Some(mut mandrop_line) = read_newln(
 		fd as RawFd,
 		&mut count,
 		&mut (static_buffer[fd]),
 		Option::None,
 	) {
-		let cstr_line = std::ffi::CStr::from_ptr(line as *const i8);
+		let cstr_line = std::ffi::CStr::from_ptr(mandrop_line.as_ptr() as *const i8);
 		let copy_return_line =
 			malloc((cstr_line.count_bytes() + 1) as c_ulong * ALLOC_SIZE) as *mut u8;
 		if !copy_return_line.is_null() {
 			cstr_line.clone_to_uninit(copy_return_line);
 		}
+		ManuallyDrop::drop(&mut mandrop_line);
 		copy_return_line as *mut c_char
 	} else {
 		std::ptr::null_mut::<c_char>()
